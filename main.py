96154c4cc37f8f95e27b793082a0fc35d8a37bf8
Rahul k <rahuljcet95@gmail.com>
2023-12-02 18:31:48 +0530

    small changes in db logic

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from  database import engine,SessionLocal
 from sqlalchemy.orm import Session
 from  models import Userdata , Base 
 import csv
-from io import StringIO
+from fastapi import HTTPException
 
 
 
 app=FastAPI()
 session = None  
 templates = Jinja2Templates(directory="Template")
-# Userdata.Base.metadata.create_all(bind=engine)
+
 Userdata.metadata.create_all(bind=engine)
 
 def get_db():
@@ -28,24 +28,33 @@ async def DisplayPage(request: Request, db: Session = Depends(get_db)):
 
 @app.post("/upload")
 async def CsvUpload(file: UploadFile = File(...), db: Session = Depends(get_db)):
-    Files_data = await file.read()
+    try:
+        Files_data = await file.read()
 
-    # Process the CSV file
-    csv_data =  Files_data.decode("utf-8").split("\n")
-    csv_reader = csv.reader(csv_data)
+        # Process the CSV file
+        csv_data = Files_data.decode("utf-8").split("\n")
+        csv_reader = csv.reader(csv_data)
 
-    # Skip the header row
-    header = next(csv_reader)
+        # Skip the header rows
+        header = next(csv_reader)
 
-    # Insert data into the database
-    for row in csv_reader:
-        if row:
-            name, age = row
-            user = Userdata(Name=name, Age=int(age))
-            db.add(user)
-    db.commit()
+        # Insert data into the database
+        for row in csv_reader:
+            if row:
+                name, age = row
+                existing_user = db.query(Userdata).filter(Userdata.Name == name, Userdata.Age == int(age)).first()
 
-    return {"filename": file.filename}
+                if existing_user is None:
+                    user = Userdata(Name=name, Age=int(age))
+                    db.add(user)
+
+        db.commit()
+
+        return {"filename": file.filename}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error processing CSV file: {str(e)}")
+
+ 
 
 
 
